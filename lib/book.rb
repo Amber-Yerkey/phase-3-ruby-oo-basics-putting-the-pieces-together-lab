afea57a8ff661b0e5e0cb8710ee2eeb672525308
Amber Frantz <amber.k.frantz@mgail.com>
2022-07-22 11:16:34 -0400

    Completed Assingment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+    attr_accessor :title, :author
+    # attr_accessor :author
+    attr_accessor :page_count, :genre
+    # attr_writer :genre
 
+    def initialize(title = "And Then There Were None")
+        @title = title  
+    end
 
+    # def page_count
+    #     272
+    # end
+
+    # def genre
+    #     "Mystery"
+    # end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+# book = Book.new("And Then There Were None")
+# book.page_count
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,35 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    # attr_accessor :color
+    # attr_accessor :size
+    # attr_accessor :material
+    # attr_accessor :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    # def color
+    #     "red"
+    # end
+
+    # def size
+    #     9.5
+    # end
+
+    # def material
+    #     "suede"
+    # end
+
+    # def condition(condition = "tattered")
+    #     @condition = condition
+    # end
+
+    def cobble
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+    end
+
+
+end
